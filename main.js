0b36a2bc56ed067003bf1325d65ff9c84de52e2e
Crypto-Advisor <adriennavarre@hotmail.com>
2022-03-30 00:43:59 -0400

    progress

--- main.js
+++ main.js
@@ -4,3 +4,52 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+let field = [[]]
+
+class Field {
+
+    Field(field){
+        this.field = field;
+    }
+    
+    print(){
+        for(let i = 0;i<field.length;i++){
+            console.log(field[i].join(''))
+        }
+    }
+
+    readInput(input){
+        if(input === 'u'){
+
+        }
+        else if(input === 'd'){
+            
+        }
+        else if(input === 'l'){
+            
+        }
+        else if(input === 'r'){
+            
+        } else{
+            console.log("invalid input ( u = up , d = down, l = left, r = right )")
+        }
+    }
+
+    movePlayer(){
+        
+
+    }
+
+    win(){
+        console.log('you lost.')
+        break
+    }
+
+    lose(){
+        console.log('you won.')
+        break
+    }
+    
+}
+
